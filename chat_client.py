ff3d411b76af106f2c175bd9236873227a603080
weiserhase <44242102+weiserhase@users.noreply.github.com>
2019-11-04 14:04:12 +0100

    Add files via upload

---
+++ chat_client.py
@@ -0,0 +1,25 @@
+import socket
+import threading
+ip = "10.1.20.107"
+port = 50000
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((ip, port))
+
+def input_thread():
+    while True:
+        anwser = s.recv(1024)
+        if not anwser:
+            s.close()
+            break
+        print(answer.decode())
+    
+t = threading.Thread(target=input_thread)
+t.start()
+
+try:
+    while True:
+        message = input(">> ")
+        s.send(message.encode())    
+finally:
+    s.close()
---
+++ socket_chat.py
@@ -0,0 +1,34 @@
+import socket
+import threading
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(("10.1.20.107", 50000))
+s.listen(1)
+
+connections = {}
+
+class recv_thread (threading.Thread):
+    def __init__(self, addr):
+        threading.Thread.__init__(self)
+        self. addr = addr
+    def run(self):
+        while True: 
+            data = connections[self.addr].recv(1024)
+            print("[{}] {}".format(self.addr, data.decode()))
+            for key, value in connections.items():
+                if key != str(self.addr): 
+                    value.send(("[{}] {}".format(self.addr, data.decode())).encode())
+
+try:
+    while(True):
+        print('in while')
+        komm, addr = s.accept()
+        clientIp = addr[0]
+        connections[clientIp] = komm
+        print('before thread init')
+        newT = recv_thread(clientIp)
+        print('before thread start')
+        newT.start()
+        print('after thread start')
+finally:
+    s.close()
